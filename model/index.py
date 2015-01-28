83050c2d235dce69e97ea62fd308dfd625be3a43
Prashant Sinha <prashnts@live.com>
2015-01-28 15:47:34 +0530

    Test

--- model/index.py
+++ model/index.py
@@ -9,11 +9,7 @@ app = application
 @app.route('/')
 def index():
     return  '''
-                <p><a href="/parks">All Parks</a></p>
-                <p><a href="/parks/park">Query by Park ID</a></p>
-                <p><a href="/parks/name">Query by Park Name</a></p>
-                <p><a href="/parks/near">Query by Coordinates</a></p>
-                <p><a href="/parks/name/near">Query by Park Name & Coordinates</a></p>
+                test!
             '''
 
 @app.route('/hello')
