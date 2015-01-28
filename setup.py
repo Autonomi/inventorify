7a019ae561b0c4a5875d809b8fb499c503c073de
Pragya Jaiswal <pragya.jswl@gmail.com>
2015-01-28 17:40:14 +0530

    Changed author name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='Warehouse', version='1.0',
       description='Autonomi Warehouse',
-      author='Your Name', author_email='ramr@example.org',
+      author='Pragya Jaiswal', author_email='ramr@example.org',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
