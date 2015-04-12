f5cefc2efe7965ee84f52da4dce0870e1e5ea92f
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-12 17:07:33 +0530

    Removed config

--- test/model/config.py
+++
@@ -1,4 +0,0 @@
-github_client_id = '8ee9ec236dfaec1256a2'
-github_client_secret = '3154c945b7263b3b36377c1b77dcd9c72eeefe4a'
-github_base_url = 'https://github.com/login/oauth/authorize'
-github_auth_url = 'https://github.com/login/oauth/'
