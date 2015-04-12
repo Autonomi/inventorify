19cf0f9b7b1824152ad7f9f78896ff01442b10e4
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-12 17:12:19 +0530

    Fixes

--- test/model/config.py
+++
@@ -1,4 +0,0 @@
-github_client_id = 'YOUR CLIENT ID'
-github_client_secret = 'YOUR CLIENT SECRET'
-github_base_url = 'https://github.com/login/oauth/authorize'
-github_auth_url = 'https://github.com/login/oauth/'
