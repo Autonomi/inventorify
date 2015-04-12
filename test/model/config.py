1096fed3c0590263d96321114c3e9027418d13f2
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-12 11:29:37 +0530

    .

--- test/model/config.py
+++ test/model/config.py
@@ -1,4 +1,4 @@
-github_client_id = '8ee9ec236dfaec1256a2'
-github_client_secret = '3154c945b7263b3b36377c1b77dcd9c72eeefe4a'
+github_client_id = 'YOUR CLIENT ID'
+github_client_secret = 'YOUR CLIENT SECRET'
 github_base_url = 'https://github.com/login/oauth/authorize'
 github_auth_url = 'https://github.com/login/oauth/'
